795151f17e564a1b7c085bd694b9793ee30fc684
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2024-08-07 20:42:38 -0400

    Update folder structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import { Routes, Route, Outlet } from 'react-router-dom'
-import './App.css'
 
 export default function App() {
     return (
-        <Routes>
-            <Route path='/' element={<Layout />}>
-                <Route index element={<Home />}></Route>
-                <Route path='test' element={<h1>Test</h1>}></Route>
-            </Route>
-        </Routes>
+        <>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<Home />}></Route>
+                    <Route path='test' element={<h1>Test</h1>}></Route>
+                </Route>
+            </Routes>
+        </>
     )
 }
 
