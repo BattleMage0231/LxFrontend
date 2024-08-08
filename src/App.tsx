7ef88927c55a8a5c3b59f6e91d35099e4d548c03
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2024-08-07 20:37:16 -0400

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import { useState } from 'react'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Lx</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+export default function App() {
+    return (
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route index element={<Home />}></Route>
+                <Route path='test' element={<h1>Test</h1>}></Route>
+            </Route>
+        </Routes>
+    )
 }
 
-export default App
+function Layout() {
+    return (
+        <>
+            <h1>Lx</h1>
+            <Outlet />
+        </>
+    )
+}
+
+function Home() {
+    const [count, setCount] = useState(0)
+    return (
+        <>
+            <h1>Home</h1>
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+        </>
+    )
+}
